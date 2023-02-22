9d8a7d6a41fab2da54d8a8ea56c5715c960ff85a
Meemitone <99894591+Meemitone@users.noreply.github.com>
2023-02-22 14:40:18 +0000

    Player Attached

--- Bullet Jazz 2.0/Assets/Scripts/PlayerControls.cs
+++ Bullet Jazz 2.0/Assets/Scripts/PlayerControls.cs
@@ -8,6 +8,7 @@ public class PlayerControls : MonoBehaviour, IPlayerActions
     [SerializeField] private Vector3 moveInput;
     [SerializeField] private Vector3 lookInput;
     [SerializeField] private GameObject reticule;
+    [SerializeField] private GameObject secondAimer;
     private Camera mainCam; //?
     [SerializeField]
     private controlMode mode;
@@ -15,7 +16,6 @@ public class PlayerControls : MonoBehaviour, IPlayerActions
     private bool mouseReset = false;
     [SerializeField]
     private float retDist = 3f;
-
 
     [SerializeField] private float defaultSpeed = 1f, dashSpeed = 3f, currentSpeed = 1f;
 
@@ -30,7 +30,6 @@ public class PlayerControls : MonoBehaviour, IPlayerActions
     {
         conts = new InputSystem();
         conts.Player.SetCallbacks(this);
-
     }
 
     // Start is called before the first frame update
@@ -79,13 +78,15 @@ public class PlayerControls : MonoBehaviour, IPlayerActions
         }
         float dotA;
         float dotB;
-        dotA = Vector3.Angle((Vector3.up + Vector3.right).normalized, reticule.transform.localPosition.normalized); //Compare ret position with up and right (the default angle of the sprite)
-        dotB = Vector3.Dot((Vector3.down + Vector3.right).normalized, reticule.transform.localPosition.normalized); //Compare also with down right (90 degrees clockwise)
+        dotA = Vector3.Angle((Vector3.right).normalized, reticule.transform.localPosition.normalized); //Compare ret position with up and right (the default angle of the sprite)
+        dotB = Vector3.Dot((Vector3.down).normalized, reticule.transform.localPosition.normalized); //Compare also with down right (90 degrees clockwise)
 
         if (dotB > 0)
             dotA *= -1;//based on the dotB value, determine the direction of the rotation
         reticule.transform.localRotation = Quaternion.identity;
         reticule.transform.Rotate(Vector3.forward, dotA); //apply the sum rotation
+        secondAimer.transform.localRotation = Quaternion.identity;
+        secondAimer.transform.Rotate(Vector3.forward, dotA); //apply the sum rotation
     }
 
     private void FixedUpdate()
