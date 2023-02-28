49411f55058324e51cac08005b2bff926f11ea6c
DeeeAich <donnachadavylohan@gmail.com>
2023-02-28 14:07:55 +0000

    REadded and didn't fuck it up this time

---
+++ Bullet Jazz 2.0/Assets/Donnacha/EnemyFire.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFire : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject prefabBullet;
+
+    private EnemyMovement enemyMove;
+
+    private void Awake()
+    {
+        enemyMove = GetComponent<EnemyMovement>();
+        target = FindObjectOfType<PlayerControls>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BeatHappened()
+    {
+
+        if (enemyMove.myMode == EnemyMovement.EnemyMode.Strafe && BeatSender.GiveInstance().beatCount == 0) 
+        {
+
+            enemyMove.myMode = EnemyMovement.EnemyMode.Fire;
+            Invoke(nameof(ShootBullet), BeatSender.GiveInstance().secondsPerBeat);
+        }
+        else if(GetComponent<NavMeshAgent>().remainingDistance > enemyMove.range)
+        {
+            GetComponent<EnemyMovement>().myMode = EnemyMovement.EnemyMode.Chase;
+        }
+        else
+        {
+            enemyMove.myMode = EnemyMovement.EnemyMode.Strafe;
+        }
+
+    }
+
+    private void ShootBullet()
+    {
+
+        GameObject currentBullet = GameObject.Instantiate(prefabBullet, transform.position, transform.rotation);
+        currentBullet.GetComponent<Rigidbody>().velocity = transform.forward * 8;
+
+        Destroy(currentBullet, 2);
+    }
+}
