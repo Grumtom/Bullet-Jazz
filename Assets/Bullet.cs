195eb2e6740f1fa9bc4b903fb129fb665d770787
Meemitone <99894591+Meemitone@users.noreply.github.com>
2023-03-22 06:59:08 +0000

    Player FireOnBeat

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gun_Script : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
+    private Rigidbody myRB;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 vel = transform.right*speed;
+        myRB.velocity = vel;
     }
 }
