74d76b63f061105fabccfe6c5653945713eb7b6d
Meemitone <99894591+Meemitone@users.noreply.github.com>
2023-04-25 10:34:48 +0100

    Beat Bullet Size and colour, no double shot
    still working on reticule fix

--- Assets/Kate/Gun_Script.cs
+++ Assets/Kate/Gun_Script.cs
@@ -7,13 +7,13 @@ public class Gun_Script : MonoBehaviour
 
     [SerializeField] private GameObject bullet;
     [SerializeField] private GameObject beatBullet;
-
+    [SerializeField] private GameObject target;
     [SerializeField] private float shotSpeed;
     [SerializeField] private float beatShotSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Reticule Pivot");
     }
 
     // Update is called once per frame
@@ -30,12 +30,14 @@ public class Gun_Script : MonoBehaviour
             shot = Instantiate(beatBullet, transform.position, transform.rotation, transform);
             Bullet bulletDeets = shot.GetComponent<Bullet>();
             bulletDeets.speed = beatShotSpeed;
+            //shot.transform.rotation = Quaternion.LookRotation(target.transform.position - shot.transform.position);
         }
         else
         {
             shot = Instantiate(bullet, transform.position, transform.rotation, transform);
             Bullet bulletDeets = shot.GetComponent<Bullet>();
             bulletDeets.speed = shotSpeed;
+            //shot.transform.rotation = Quaternion.LookRotation(target.transform.position - shot.transform.position);
         }
         if (shot != null)
             shot.transform.parent = null;
